463957ecb6c6dc9d39179c2c91b7fb29eb50ffcd
Jeff <dukedvl@gmail.com>
2020-04-08 01:35:33 -0400

    Add project files.

---
+++ NewsDashboard/News/News_VM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsDashboard.News
+{
+    class News_VM
+    {
+    }
+}
---
+++ NewsDashboard/News/News_View.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewsDashboard.News
+{
+    /// <summary>
+    /// Interaction logic for News_View.xaml
+    /// </summary>
+    public partial class News_View : UserControl
+    {
+        public News_View()
+        {
+            InitializeComponent();
+        }
+    }
+}
